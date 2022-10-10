dec65b568bb05e0a9e836133c7d5001a7b8f1bef
Alex Dowdney <alexdowdney@gmail.com>
2022-10-10 17:31:35 -0400

    Initial Setup

---
+++ index.js
@@ -0,0 +1,47 @@
+const DiscordJS = require('discord.js')
+const fs = require('fs')
+const Intents = DiscordJS.Intents
+const Collection = DiscordJS.Collection
+const dotenv = require('dotenv')
+
+
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+
+    ]
+
+})
+// github test
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
+
+const commands = [];
+
+client.commands = new Collection();
+client.buttons = new Collection();
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+    commands.push(command.data.toJSON());
+    client.commands.set(command.data.name, command);
+}
+
+const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith('.js'));
+
+
+for(const file of eventFiles){
+    const event = require(`./events/${file}`);
+
+    if(event.once) {
+        client.once(event.name, (...args) => event.execute(...args, commands));
+    }
+    else{
+        client.on(event.name, (...args) => event.execute(...args, commands));
+    }
+}
+
+
+client.login(process.env.TOKEN);
